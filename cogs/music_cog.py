f955138946e2cdd5108cffc3adae7a0260aef666
Marcin Chiliński <marcin.chilinski.mobile@o2.pl>
2023-09-29 20:26:36 +0200

    Add MusicCog

--- cogs/music_cog.py
+++ cogs/music_cog.py
@@ -1,9 +1,21 @@
 import discord
+import asyncio
 from discord.ext import commands
 
-
-from youtube_dl import YoutubeDL
+from yt_dlp import YoutubeDL
 import time
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+youtubedl_options = {
+    'format': 'bestaudio/best',
+}
+
+url = 'https://www.youtube.com/watch?v=ag8XcMG1EX4&ab_channel=CrowdedHouseVEVO'
+
+
+Ctx = commands.Context
 
 
 
@@ -13,7 +25,23 @@ class Music_cog(commands.Cog):
         self.vc = discord.VoiceClient
         self.bot = bot
 
-    @commands.command("aaa")
-    async def aaa(self, ctx):
-  
-        await ctx.send("Playing")
+    @commands.command()
+    async def play(self, ctx : Ctx):
+        message = ctx.message.content.split(" ")
+        url = message[1]
+        voice = await ctx.message.author.voice.channel.connect(timeout=10000)
+        # clients[voice.guild.id] = voice
+        ydl = YoutubeDL(youtubedl_options)
+
+        loop = asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ydl.extract_info(url, download=False))
+        await ctx.send("Plaing...")
+        # clients[voice.guild.id].play(discord.FFmpegPCMAudio(data['url'], **ffmpeg_options, executable="ffmpeg"))
+        voice.play(discord.FFmpegPCMAudio(data['url'], **ffmpeg_options, executable="ffmpeg"))
+        
+        
+
+        while voice.is_playing():
+            await asyncio.sleep(1)
+
+        await voice.disconnect()
--- test.py
+++ test.py
@@ -1,21 +1,20 @@
-from yt_dlp import YoutubeDL
 import asyncio
 import time
-url = 'https://www.youtube.com/watch?v=h_D3VFfhvs4&ab_channel=michaeljacksonVEVO'
-youtubedl_options = {
-    'format': 'bestaudio/best',
-}
-ydl = YoutubeDL(youtubedl_options)
 
 
-
-
-async def run():
-    loop = asyncio.get_event_loop()
-    data = await loop.run_in_executor(None, lambda: ydl.extract_info(url, download=False))
+async def some_async_function():
     while True:
-        print(data['url'])
-        print('----------------------')
-        time.sleep(1)
+        print("Async function is running...")
+        await asyncio.sleep(1)
 
-asyncio.run(run())
+def main():
+    loop = asyncio.get_event_loop()
+
+    # Run the asyncio function indefinitely
+    loop.create_task(some_async_function())
+
+    loop.run_forever()
+  
+
+if __name__ == "__main__":
+    main()
